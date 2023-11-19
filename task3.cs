5c1d459355f2f05ea6aaf95a9f19db7a3d4d6519
RomanovCopy <RomanovCopy@gmail.com>
2023-11-19 10:57:45 +0300

    end

--- task3.cs
+++
@@ -1,3 +0,0 @@
-/* Запретить любой входящий трафик с IP 3.4.5.6.*/
-sudo iptables -A INPUT -s 3.4.5.6 -j DROP
-sudo iptables-save > /etc/iptables.rules
---
+++ код HTML страницы с использованием PHP.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Romanov SM PHP-FPM Page</title>
+</head>
+<body>
+    <h1>Romanov SM PHP-FPM</h1>
+    <p>Today's date is: <?php echo date("Y-m-d"); ?></p>
+</body>
+</html>
