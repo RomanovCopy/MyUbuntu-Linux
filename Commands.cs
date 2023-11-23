01a093783387b8b9ced92eacdd501e7108be64af
RomanovCopy <RomanovCopy@gmail.com>
2023-11-23 18:22:34 +0300

    fix

--- Commands.cs
+++ Commands.cs
@@ -8,7 +8,7 @@ sudo apt install docker.io
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-Создаем файл конфигурации для docker-compose(приведен отдельным файлом)
+Создаем файл конфигурации для docker-compose(приведен отдельным файлом c измененным разрешение, т.к .yml GitHub не понимает)
 
 nano /etc/docker/docker-compose.yml
 
